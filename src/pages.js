967366567ac554ea813bd8471ad785308328b832
dsgsantos1 <dsgsantos1@gmail.com>
2021-01-17 12:37:42 -0400

    feat: login page and login method
    loginPage use a random page because the page is in dev
    login trata os dados para login e para criaçao e contas

--- src/pages.js
+++ src/pages.js
@@ -1,5 +1,36 @@
 function home(req, res) {
-	res.render("index.html")
+	return res.render("index.html")
+}
+
+function pageLogin(req, res) {
+	// Ainda não o nome da pagina, então ficara essa por enquanto
+	res.render('page_name')
+}
+
+function login(req, res) {
+
+	if (req.body.isUser) {
+		const login_query = {
+			login: req.body.login,
+			password: req.body.password
+		}
+	}else{
+
+		const data = {
+			name: req.body.name,
+			user: req.body.user,
+			email:  req.body.email,
+			password: req.body.password,
+			tel: req.body.tel,
+			bio: req.body.bio
+		}
+
+		const db = require('./database/db.js')
+
+		db.addUser(data)
+	}
+
+	return res.redirect('/mentorias')
 }
 
 module.exports = {
