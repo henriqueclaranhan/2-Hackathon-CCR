e95c8c7e5c9ae44f619555cabcd374ff18d74755
dsgsantos1 <dsgsantos1@gmail.com>
2021-01-17 19:01:56 -0400

    add: login request by fetch

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,6 +1,27 @@
-indexBtn = document.querySelector("#return");
+var indexBtn = document.querySelector("#return");
+var button = document.querySelector("#continue-button")
 loginLink = '/'
+
+const loginRequest = async (event) => {
+	const form = {
+		isUser: true,
+		login: document.querySelector('#userlogin').value,
+		password: document.querySelector("#password").value
+	}
+
+	const response = await fetch('http://localhost:3000/login', {
+		method:'POST',
+		headers: {
+      	'Content-Type': 'application/json'
+    	},
+		body: JSON.stringify(form)
+	})
+}
+
+button.addEventListener('click',loginRequest)
 
 indexBtn.addEventListener("click", () =>{
     window.location.href = loginLink
 })
+
+
