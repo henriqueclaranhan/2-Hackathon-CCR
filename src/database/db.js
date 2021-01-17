d3629a57048b63a8c334ad92f5940021d6e3b579
dsgsantos1 <dsgsantos1@gmail.com>
2021-01-17 13:01:04 -0400

    feat: hash password

--- src/database/db.js
+++ src/database/db.js
@@ -1,15 +1,23 @@
 const { JsonDB } = require('node-json-db');
 const { Config } = require('node-json-db/dist/lib/JsonDBConfig')
 const initDb = require('./createShowHow.js')
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const db = new JsonDB(new Config("showhow", true, false, '/'));
 initDb(db)
+
+function hash(password){
+	const hash_password = await bcrypt.hash(password, saltRounds)
+
+	return hash_password
+}
 
 function addUser({name, user, email, password, tel, bio}) {
 	// Get the new number to add
 	const lastAdd = db.getData('/mentores').length ;
 
-	const user_login = {user, email, password}
+	const user_login = {user, email, password: hash(password)}
 	const perfil = {name, bio, tel}
 
 	db.push(`/mentores[${lastAdd}]`, perfil)
