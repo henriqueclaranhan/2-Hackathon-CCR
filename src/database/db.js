7fe91eb04d4b17cdd0c02992fc64eafea72b0744
dsgsantos1 <dsgsantos1@gmail.com>
2021-01-17 13:10:45 -0400

    fix: implementação do hash
    não é possivel usar com await, sendo obrigatorio usar callback

--- src/database/db.js
+++ src/database/db.js
@@ -7,21 +7,19 @@ const saltRounds = 10;
 const db = new JsonDB(new Config("showhow", true, false, '/'));
 initDb(db)
 
-function hash(password){
-	const hash_password = await bcrypt.hash(password, saltRounds)
-
-	return hash_password
-}
-
 function addUser({name, user, email, password, tel, bio}) {
 	// Get the new number to add
 	const lastAdd = db.getData('/mentores').length ;
 
-	const user_login = {user, email, password: hash(password)}
+
 	const perfil = {name, bio, tel}
 
 	db.push(`/mentores[${lastAdd}]`, perfil)
-	db.push(`/users[${lastAdd}]`, user_login)
+
+	bcrypt.hash(password, saltRounds, function(err, hash) {
+    	const user_login = {user, email, password: hash}
+    	db.push(`/users[${lastAdd}]`, user_login)
+	});
 
 }
 
